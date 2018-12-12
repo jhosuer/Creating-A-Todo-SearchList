6cbfaec0d6ebbc0bcbea4d74c98098362e2bcf24
Jhosuer <jumbojoshua91@gmail.com>
2018-12-12 11:57:02 +0100

    added the css and readme file

--- todo.js
+++ todo.js
@@ -1,7 +1,24 @@
 $(init);
 
 function init() {
+    let url = 'https://jsonplaceholder.typicode.com/todos';
+   
     $('#todo').on('keypress', function(){
-       alert('yea'); 
+        console.log($('#todo').val().split().splice(0,3));
+        $.get(url, function(response) {
+            
+            let userInput = Number($('#todo').val());
+            if((userInput != '') && (typeof userInput == 'number')) {
+                $('#searchResult').html(response[$('#todo').val() - 1].title);   
+            }
+            else if((userInput != '') && (typeof userInput == 'string')) {
+                // if(userInput.includes(response.splice(0,3))) {
+
+                // }
+            }
+            else if((userInput == '')){
+                $('#searchResult').html(''); 
+            }
+        });
     });
 }
